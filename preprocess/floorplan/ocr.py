b19f3dbe6efac03faf360dc04fce5cb7e38defc5
Ishant Aggarwal <iagg1946@gmail.com>
2024-09-18 23:06:27 +0100

    silent paddle

--- preprocess/floorplan/ocr.py
+++ preprocess/floorplan/ocr.py
@@ -24,8 +24,8 @@ class OcrEngine:
         self.setup_ocr()
 
     def setup_ocr(self) -> None:
-        self.engine = PaddleOCR(use_angle_cls=True, lang='en', det_lang='ml', debug=False)
-        self.engine_block = PaddleOCR(use_angle_cls=True, lang='en', det_lang=None, debug=False)
+        self.engine = PaddleOCR(use_angle_cls=True, lang='en', det_lang='ml', show_log=False)
+        self.engine_block = PaddleOCR(use_angle_cls=True, lang='en', det_lang=None, show_log=False)
         # pytesseract.pytesseract.tesseract_cmd = OCR_ENGINE
 
     @staticmethod
